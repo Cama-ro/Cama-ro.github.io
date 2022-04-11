5e1cde7a54e923ce748dddd1aa5992beb8803303
Cama-ro <86301716+Cama-ro@users.noreply.github.com>
2022-04-11 13:17:59 +0200

    Delete 1-pobierz-dane-programisty directory

--- 1-pobierz-dane-programisty/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $(".button").click(function () {
-    $.getJSON("https://jsonplaceholder.typicode.com/users/1", function (data) {
-      console.log(data);
-      $("#dane-programisty").text(JSON.stringify(data));
-    });
-  });
-});
